573b762f6f83e33fee16dd3feb65be5bcb4c4501
LinhDev610 <binbicute1997@gmail.com>
2026-01-16 14:50:17 +0700

    chore: lombok

--- src/main/java/com/linhdev/identityservice/dto/request/UserCreationRequest.java
+++ src/main/java/com/linhdev/identityservice/dto/request/UserCreationRequest.java
@@ -1,58 +1,24 @@
 package com.linhdev.identityservice.dto.request;
 
-import com.linhdev.identityservice.exception.ErrorCode;
 import jakarta.validation.constraints.Size;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDate;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserCreationRequest {
     @Size(min = 3, message = "USERNAME_INVALID")
-    private String username;
+    String username;
 
     @Size(min = 8, message = "PASSWORD_INVALID")
-    private String password;
-    private String firstName;
-    private String lastName;
+    String password;
+    String firstName;
+    String lastName;
 
-    private LocalDate dob;
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    LocalDate dob;
 }
