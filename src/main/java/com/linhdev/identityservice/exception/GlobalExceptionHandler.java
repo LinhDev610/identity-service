6dc5f8ebd7c795e10633f32db6fe114178105950
LinhDev610 <binbicute1997@gmail.com>
2026-01-30 14:56:57 +0700

    feat: Trích xuất thông tin từ Annotation Validate và trả về trong Exception

--- src/main/java/com/linhdev/identityservice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/linhdev/identityservice/exception/GlobalExceptionHandler.java
@@ -1,16 +1,25 @@
 package com.linhdev.identityservice.exception;
 
 import com.linhdev.identityservice.dto.request.ApiResponse;
+import jakarta.validation.ConstraintViolation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Map;
+import java.util.Objects;
 
+
+@Slf4j
 @ControllerAdvice
 // Noi define tat cac loai exception va tuong tung tung exception tra ve ntn
 public class GlobalExceptionHandler {
+
+    private static final String MIN_ATTRIBUTE = "min";
+
     @ExceptionHandler(value = Exception.class)
     ResponseEntity<ApiResponse> handlingRuntimeException(RuntimeException exception) {
         ApiResponse apiResponse = new ApiResponse();
@@ -50,18 +59,37 @@ public class GlobalExceptionHandler {
         String enumKey = exception.getFieldError().getDefaultMessage();
 
         ErrorCode errorCode = ErrorCode.INVALID_KEY;
+        Map<String, Object> attributes = null;
 
-       try {
-           errorCode = ErrorCode.valueOf(enumKey);
-       } catch (IllegalArgumentException e) {
+        try {
+            errorCode = ErrorCode.valueOf(enumKey);
 
-       }
+            // Lay thong tin attribute trong exception khi validate
+            var constraintViolation = exception.getBindingResult()
+                    .getAllErrors().getFirst().unwrap(ConstraintViolation.class);
+
+            attributes = constraintViolation.getConstraintDescriptor().getAttributes();
+
+            // log.info("Attribute: " + String.valueOf(attributes));
+
+        } catch (IllegalArgumentException e) {
+
+        }
 
         ApiResponse apiResponse = new ApiResponse();
 
         apiResponse.setCode(errorCode.getCode());
-        apiResponse.setMessage(errorCode.getMessage());
+        apiResponse.setMessage(Objects.nonNull(attributes)
+                ? mapAttribute(errorCode.getMessage(), attributes)
+                : errorCode.getMessage()
+        );
 
         return ResponseEntity.badRequest().body(apiResponse);
+    }
+
+    private String mapAttribute(String message, Map<String, Object> attributes) {
+        String minValue = attributes.get(MIN_ATTRIBUTE).toString();
+
+        return message.replace("{" + MIN_ATTRIBUTE + "}", minValue);
     }
 }
